bf63f597afb8fa7667f2dff0c72caa33507ef0a3
Naoman Saeed <naoman.saeed@gmail.com>
2026-05-21 05:52:00 +0500

    feat: turning readFile() operation into an asynchronous method.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import http from 'node:http';
-import fs from 'node:fs';
+import fs from 'node:fs/promises';
 //import path from 'node:path';
 import { testPath } from './utils/testPath.js';
 import { serveStatic } from './utils/serveStatic.js';
@@ -8,19 +8,29 @@ const PORT = 3001;
 
 const __dirname = import.meta.dirname;
 
-const server = http.createServer((req,res) => {
+const server = http.createServer(async (req,res) => {
+
+    const pathToResource = serveStatic(__dirname);
+
+    const content = await fs.readFile(pathToResource, 'utf-8');
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html');
+    res.end(content);
+
     //const pathToResource = path.join(__dirname, 'public', 'index.html');
     //const output = `<html><h1>Server Up!</h1></html>`;
     //testPath();
     //console.log(`testPath: ${testPath()}`);
     //console.log(serveStatic(__dirname));
 
-    const pathToResource = serveStatic(__dirname);
+    
 
     // Reads the contents of a static file synchronously.
     //const content = fs.readFileSync(pathToResource,'utf-8');
 
-    fs.readFile(pathToResource, 'utf-8', (error, content) => {
+    //Such nested code can lead to callBack() hell
+  /*  fs.readFile(pathToResource, 'utf-8', (error, content) => {
         if (error) {
             console.log(error);
             return;
@@ -28,7 +38,7 @@ const server = http.createServer((req,res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type','text/html');
         res.end(content);
-    })
+    }); */
 
     //res.statusCode = 200;
     //res.setHeader('Content-Type','text/html');
